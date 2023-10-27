09c0696b2f35e5a720dd78b38daed60f72029320
arta fallahpoor <102357381+artafp@users.noreply.github.com>
2023-10-27 09:58:16 +0330

    Update decimalNumber.py

--- decimalNumber.py
+++ decimalNumber.py
@@ -1,86 +1,49 @@
-from tabulate import tabulate
+"""
+power by artafp
+url : artafp.ir , gethub.com/artafp
+"""
+class decimalNumber:
+    def __init__(self,number) :
+        self.correct = number.split(".")[0]
+        self.decimal = number.split(".")[1]
+    def __str__ (self):
+        return f"{self.correct}.{self.decimal}"
+    def __add__ (self,decimalN):
+        addCorrect = str(int(decimalN.correct)+int(self.correct))
+        DN1 ,DN2 = decimalN.decimal,self.decimal
+        while(len(DN1) != len(DN2)):
+            if(len(DN1)>len(DN2)):
+                DN2 += "0"
+            if(len(DN2)>len(DN1)):
+                DN1 += "0"
+        addDecimal = str(int(DN1)+int(DN2)) 
+        D = 0
+        newAddDecimal = addDecimal
+        if(len(addDecimal)>len(DN1)):
+            newAddDecimal = ""
+            D = 1
+            for i in range(1, len(addDecimal), 1):
+                newAddDecimal += addDecimal[i]
+        return decimalNumber(f"{int(addCorrect)+D}.{newAddDecimal}") 
+    def __mul__ (self,decimalN):
+        mulSelf = f"{self.correct}{self.decimal}"
+        mulDecimalN = f"{decimalN.correct}{decimalN.decimal}"
+        mul = str(int(mulDecimalN) * int(mulSelf))
+        retMulCorrect = ''
+        retMulDecimal = ''
+        lenDecimalMUl = len(self.decimal) + len(decimalN.decimal)
+        for i in range(0,len(mul)-lenDecimalMUl,1):
+            retMulCorrect += mul[i]
+        for i in range(len(mul)-lenDecimalMUl,len(mul),1):
+            retMulDecimal += mul[i]
+        return decimalNumber(f"{retMulCorrect}.{retMulDecimal}") 
 
-n = int(input("The number of rows of the matrix 1 :"))
-m = int(input("The number of columns of the matrix 1 :"))
-n2 = int(input("The number of rows of the matrix 2 :"))
-m2 = int(input("The number of columns of the matrix 2 :"))
-def normalToSparse (n,m):
-    while n<2 and m<2 :
-        n = int(input("The number of rows and columns of the matrix :"))
-    matrix = []
-    matrixSparse = [[n,m,0]]
-    for i in range(0,n,1):
-        rows = []
-        for j in range(0,m,1):
-            print("The row",i ,"of the column",j,":+>")
-            inp=int(input())
-            if(inp!=0):
-                matrixSparse[0][2] += 1
-                matrixSparse.append([i,j,inp])
-            rows.append(inp)
-        matrix.append(rows)
-    return matrixSparse        
-
-class saveSparse:
-    def __init__(self,val) :
-        self.arrMS=val
-    def __add__(self,newMS):
-        if(self.arrMS[0][0] == newMS.arrMS[0][0]):
-            if(self.arrMS[0][1]==newMS.arrMS[0][1]):
-                newArrSumSparse=newMS.arrMS
-                for i in range(1,len(self.arrMS),1):
-                    notFind = True
-                    for j in range(1,len(newMS.arrMS),1):
-                        
-                        if(self.arrMS[i][0] == newMS.arrMS[j][0] and self.arrMS[i][1] == newMS.arrMS[j][1]):
-                            newArrSumSparse[i][2] = newMS.arrMS[j][2] +  self.arrMS[i][2]
-                            notFind = False
-                    if(notFind):
-                        newArrSumSparse[0][2] = newArrSumSparse[0][2] + 1
-                        newArrSumSparse.append(self.arrMS[i])
-                return newArrSumSparse            
-            else: 
-                return -1
-        else: 
-            return -1
-    # def __mul__(self,newMS):
-    #     martrix=newMS.arrMS
-    #     if(len(self.arrMS) > len(newMS.arrMS) ):
-    #         martrix = self.arrMS
-    #     if(self.arrMS[0][0] == newMS.arrMS[0][1]):
-    #           arryFinal = [[self.arrMS[0][0] , newMS.arrMS[0][1],0]]
-    #           for k in range(1,len(martrix),1):
-    #               for k2 in range(1,len(newMS.arrMS),1):
-    #                   if(martrix[k][0]==newMS.arrMS[k2][1] and martrix[k][1]== newMS.arrMS[k2][0]):
-    #                       arryFinal[0][2] = arryFinal[0][2] + 1
-    #                       if(len(arryFinal)<2):
-    #                           arryFinal.append([martrix[k][0],newMS.arrMS[k2][1],martrix[k][2]*newMS.arrMS[k2][2]])
-    #                       for D in range(1,len(arryFinal),1):   
-    #                           if(arryFinal[D][0]==martrix[k][0] and arryFinal[D][1] == newMS.arrMS[k2][1]):
-    #                               arryFinal[D][2] += martrix[k][2]*newMS.arrMS[k2][2]        
-    #                           else:
-    #                               arryFinal.append([martrix[k][0],newMS.arrMS[k2][1],martrix[k][2]*newMS.arrMS[k2][2]]) 
-    #           return arryFinal                   
-    #     else: 
-    #       return -1
-    def transpose(self):
-        matrisN = [[self.arrMS[0][1],self.arrMS[0][0],self.arrMS[0][2]]]
-        for i in range(1,len(self.arrMS),1):
-            matrisN.append([self.arrMS[i][1],self.arrMS[i][0],self.arrMS[i][2]])
-        return  matrisN   
-print("matrix 1")    
-matrix = normalToSparse(n,m)
-print("matrix 2")
-matrix2 = normalToSparse(n2,m2)
-clMS1=saveSparse(matrix)
-clMS2=saveSparse(matrix2)
-sums= clMS1+clMS2
-print("is sums matrixSpars")
-print(tabulate(sums,tablefmt="grid") )
-sumNew = saveSparse(sums)
-print("is sums transpose")
-print(tabulate(sumNew.transpose(),tablefmt="grid"))
-
-#  tabulate(matrix,tablefmt="grid")
-
-
+print("Please enter the numbers correctly")
+number1 = input("type number 1 :")
+number2 = input("type number 2 :")
+newNumberDecimal1 = decimalNumber(number1)
+newNumberDecimal2 = decimalNumber(number2)
+print("number 1 :",newNumberDecimal1)
+print("number 2 :",newNumberDecimal2)
+print("sum :",newNumberDecimal1+newNumberDecimal2)
+print("mul :",newNumberDecimal1*newNumberDecimal2)
