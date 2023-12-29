8b90c7004807dfdf30127579bb5e2d1a0041a84e
arta fallahpoor <102357381+artafp@users.noreply.github.com>
2023-12-29 20:23:09 +0330

    Update Scientific Calculator.py

--- پروژه های ساختمان داده/Scientific Calculator.py
+++ پروژه های ساختمان داده/Scientific Calculator.py
@@ -29,13 +29,13 @@ class Stack:
         if self.isEmpty():
             raise Exception("Peeking from an empty stack")
         return self.head.next.value
- 
+
     def push(self, value):
         node = Node(value)
         node.next = self.head.next
         self.head.next = node
         self.size += 1
- 
+
     def pop(self):
         if self.isEmpty():
             raise Exception("Popping from an empty stack")
@@ -46,23 +46,25 @@ class Stack:
 
 class Calculate:
     def __init__(self, equation:str):
+        # الگوهای بازنویسی توابع ریاضی مانند cos، sin و log را تشخیص داده و جایگزین مقادیر حاصل شده می‌کند.
         pattern = re.compile(r'cos\(([-+]?(?:\d*\.*\d+))\)')
         matches = list(pattern.finditer(equation))
-        pattern = re.compile(r'sin\(([-+]?(?:\d*\.*\d+))\)')
+        pattern = re.compile(r'sin\((([-+]?(?:\d*\.*\d+)))\)')
         matches = list(pattern.finditer(equation)) + matches
-        pattern = re.compile(r'log\(([-+]?(?:\d*\,*.*\d+))\)')
+        pattern = re.compile(r'log\(([-+]?(?:\d*\.*\d+)), ([-+]?(?:\d*\.*\d+))\)')
         matches = list(pattern.finditer(equation)) + matches
         if matches:
             matches = [match.group(0) for match in matches]
             for i in matches:
                 equation = equation.replace(i, str(eval("math."+i)))
         self.equation = equation
-        
+
     def InfixToPostfix(self):
-        Operators = set(['+', '-', '*', '/', '(', ')', '^'])  
+        # تابع تبدیل عبارت انفیکس به پستفیکس
+        Operators = set(['+', '-', '*', '/', '(', ')', '^'])
         Priority = {'+':1, '-':1, '*':2, '/':2, '^':3}
         stack = Stack()
-        output = [] 
+        output = []
         put = ''
 
         for character in self.equation:
@@ -77,7 +79,7 @@ class Calculate:
                     output.append(put)
                     put=''
                 while not(stack.isEmpty()) and stack.peek()!='(':
-                    output+=stack.pop()
+                    output.append(stack.pop())
                 stack.pop()
 
             else:
@@ -85,18 +87,19 @@ class Calculate:
                     output.append(put)
                     put=''
                 while not(stack.isEmpty()) and stack.peek()!='(' and Priority[character]<=Priority[stack.peek()]:
-                    output+=stack.pop()
+                    output.append(stack.pop())
                 stack.push(character)
         if put:
             output.append(put)
         while not(stack.isEmpty()):
-            output+=stack.pop()
-        
+            output.append(stack.pop())
+
         self.postfix = output
         return output
 
     def compute(self):
-        Operators = set(['+', '-', '*', '/', '^'])  
+        # تابع محاسبه ارزش عبارت پستفیکسی
+        Operators = set(['+', '-', '*', '/', '^'])
         postfix = self.InfixToPostfix()
         stack = Stack()
         for character in postfix:
@@ -106,16 +109,15 @@ class Calculate:
                 a = float(stack.pop())
                 b = float(stack.pop())
                 stack.push(eval(f"{b}{character}{a}"))
-        return(stack.pop())
+        return stack.pop()
 
-            
-            
-    
-
-                
-
-
-# calculate(str(input("")))
-Calculate("3*cos(0)+cos(1)+sin(0)+log(2, 2)")
-a = Calculate("1+2*(3/(4+5))*6+cos(0)")
+# ---------- Main ----------#
+# ایجاد یک شیء از کلاس Calculate با یک عبارت ریاضی
+a = Calculate("3*cos(0)+cos(1)+sin(0)+log(2, 2)")
+# چاپ نتیجه محاسبه
 print(a.compute())
+
+# ایجاد یک شیء دیگر از کلاس Calculate با یک عبارت ریاضی دیگر
+b = Calculate("1+2*(3/(4+5))*6+cos(0)")
+# چاپ نتیجه محاسبه
+print(b.compute())
