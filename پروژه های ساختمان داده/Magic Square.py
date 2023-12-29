21f9d0b59c97e228922209340d8d6d225bd745f7
arta fallahpoor <102357381+artafp@users.noreply.github.com>
2023-12-29 20:19:48 +0330

    Update Magic Square.py

--- پروژه های ساختمان داده/Magic Square.py
+++ پروژه های ساختمان داده/Magic Square.py
@@ -1,22 +1,23 @@
-def MagicSquare(n): #only works for odd numbers
-    magicSquare = [[-1 for _ in range(n)] for _ in range(n)]
-    row, col = n // 2, 0
+def MagicSquare(n):  # تعریف تابع ایجاد جادوگریده برای اعداد فردی
+    magicSquare = [[-1 for _ in range(n)] for _ in range(n)]  # ایجاد یک ماتریس n در n و مقداردهی اولیه با -1
+    row, col = n // 2, 0  # مکان شروع اعداد
 
     for i in range(1, n * n + 1):
-        magicSquare[row][col] = i
+        magicSquare[row][col] = i  # قرار دادن عدد در ماتریس
 
-        if i % n == 0:     #if magicSquare[row-1][col-1] != 0:
+        if i % n == 0:  # اگر عدد تقسیم‌پذیر بر n باشد، تغییر ستون
             col -= 1
         else:
-            row -= 1
+            row -= 1  # در غیر این صورت، تغییر سطر و ستون به طور همزمان
             col += 1
-        row = (row + n) % n
+        row = (row + n) % n  # حلقه میانبر برای مدیریت شروع از ابتدای ماتریس
         col = (col + n) % n
 
+    # چاپ جادوگریده
     for i in range(n):
         for j in range(n):
-            print(magicSquare[i][j],end=" ")
+            print(magicSquare[i][j], end=" ")
         print()
 
-#---------- Main ----------#
-MagicSquare(5)
+# ---------- Main ----------#
+MagicSquare(5)  # تابع را با n=5 فراخوانی کن
